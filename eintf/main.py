00ebc268503fc5512fa8e69d7ad4d6cca4e4ac0a
kshib <ksyko@pm.me>
2023-01-21 17:56:41 +0530

    add some endpoints

--- eintf/main.py
+++ eintf/main.py
@@ -1,6 +1,42 @@
-from extractor.hud import Hud
-from extractor.map import Map
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+from .extractor.hud import Hud
+from .extractor.map import Map
+
+app = FastAPI()
 
 
-print(Hud().active())
-print(Map().featured_downloads())
+@app.exception_handler(500)
+async def exception_handler(request, exc):
+    return JSONResponse(content={"success": False, "error": str(exc)}, status_code=500)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/huds")
+def active_huds():
+    return Hud().active()
+
+
+@app.get("/huds/outdated")
+def outdated_huds():
+    return Hud().outdated()
+
+
+@app.get("/huds/outdated/{hud}")
+def outdated_hud(hud):
+    return Hud().outdated()["data"][hud]
+
+
+@app.get("/huds/{hud}")
+def active_hud(hud):
+    return Hud().active()["data"][hud]
+
+
+@app.get("/maps/featured")
+def maps(property):
+    return Map().featured_downloads()
