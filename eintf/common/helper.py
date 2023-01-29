8b76e90f6c1cce1976a2ebe6fd966c8864154499
kshib <ksyko@pm.me>
2023-01-29 21:36:12 +0530

    add method to get unixtime

--- eintf/common/helper.py
+++ eintf/common/helper.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime, timezone
 
 
@@ -9,3 +10,6 @@ def convert_time(orig_date, orig_format):
     # IDK why but timestamps are off by a day
     return int(datetime.strptime(orig_date, orig_format).timestamp()) + 86400
 
+
+def now():
+    return int(time.time())
